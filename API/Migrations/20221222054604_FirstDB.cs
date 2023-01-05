8bb48422fe221b83c94e60140007322bb6fc85f9
Jonathan purnama halim <46183646+nattnathan@users.noreply.github.com>
2023-01-05 15:36:11 +0700

    Segment 3 Mantab
    LGTM! Gas!

---
+++ API/Migrations/20221222054604_FirstDB.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    NIK = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    BirthDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Salary = table.Column<int>(type: "int", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Gender = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.NIK);
+                    table.UniqueConstraint("AK_Employees_Email", x => x.Email);
+                    table.UniqueConstraint("AK_Employees_Phone", x => x.Phone);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Universities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Universities", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    NIK = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OTP = table.Column<int>(type: "int", nullable: false),
+                    ExpiredToken = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsUsed = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.NIK);
+                    table.ForeignKey(
+                        name: "FK_Accounts_Employees_NIK",
+                        column: x => x.NIK,
+                        principalTable: "Employees",
+                        principalColumn: "NIK",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Educations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Degree = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    GPA = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UniversityId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Educations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Educations_Universities_UniversityId",
+                        column: x => x.UniversityId,
+                        principalTable: "Universities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AccountRoles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AccountNIK = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    RoleId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AccountRoles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AccountRoles_Accounts_AccountNIK",
+                        column: x => x.AccountNIK,
+                        principalTable: "Accounts",
+                        principalColumn: "NIK",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AccountRoles_Roles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Profilings",
+                columns: table => new
+                {
+                    NIK = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    EducationId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Profilings", x => x.NIK);
+                    table.ForeignKey(
+                        name: "FK_Profilings_Accounts_NIK",
+                        column: x => x.NIK,
+                        principalTable: "Accounts",
+                        principalColumn: "NIK",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Profilings_Educations_EducationId",
+                        column: x => x.EducationId,
+                        principalTable: "Educations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AccountRoles_AccountNIK",
+                table: "AccountRoles",
+                column: "AccountNIK");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AccountRoles_RoleId",
+                table: "AccountRoles",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Educations_UniversityId",
+                table: "Educations",
+                column: "UniversityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Profilings_EducationId",
+                table: "Profilings",
+                column: "EducationId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AccountRoles");
+
+            migrationBuilder.DropTable(
+                name: "Profilings");
+
+            migrationBuilder.DropTable(
+                name: "Roles");
+
+            migrationBuilder.DropTable(
+                name: "Accounts");
+
+            migrationBuilder.DropTable(
+                name: "Educations");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Universities");
+        }
+    }
+}
--- Client/wwwroot/js/site.js
+++ Client/wwwroot/js/site.js
@@ -1,4 +1,186 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿//let judul = document.getElementById("judul");
 
-// Write your JavaScript code.
+
+//let paragraf = document.getElementsByTagName("p")[0];
+//paragraf.style.backgroundColor = "red";
+
+//let variable1 = document.getElementsByClassName("list");
+
+//judul.onclick = function () {
+//    paragraf.innerHTML = "berubah!";
+//}
+
+//let ul = document.getElementsByTagName("ul");
+//let li = ul.getElementsByTagName("li");
+
+//judul.onclick = () => {
+//    judul.style.backgroundColor = "cyan";
+//    judul.innerHTML = "saya rubah dari JS";
+//}
+
+//judul.addEventListener("click", function () {
+//    paragraf.innerHTML = "berubah!";
+//});
+
+//judul.addEventListener("click", function () {
+//    judul.style.backgroundColor = "cyan";
+//    judul.innerHTML = "saya rubah dari JS";
+//});
+
+//paragraf.addEventListener("mouseenter", function () {
+//    paragraf.innerHTML = "Mashokkkk!!!";
+//});
+
+//paragraf.addEventListener("mouseleave", function () {
+//    paragraf.innerHTML = "KELOARRR!!!";
+//});
+
+//let list3 = document.querySelector("li.list#list:nth-child(3)");
+
+//function berubah() {
+//    for (var i = 0; i < variable1["length"]; i++) {
+//        variable1[i].innerHTML = "diubah dari onclick html";
+//    }
+//}
+
+////Array
+//let array = [1, 2, 3, 4];
+////insert last
+//array.push("halo");
+//console.log(array);
+////delete last
+//array.pop();
+//console.log(array);
+////insert first
+//array.unshift("test");
+//console.log(array);
+////delete first
+//array.shift();
+//console.log(array);
+
+////array multi dimensi
+//let arrayMulti = [1, 2, 3, ['a', 'b', 'c'], true];
+//console.log(arrayMulti);
+
+//let tambah = (x, y) => { return x + y; }
+//console.log(tambah(5, 10));
+
+////array associative
+//array["asda"]
+
+//////constanta
+////const nilai = 50;
+////console.log(nilai);
+////nilai = 90;
+////console.log(nilai);
+
+
+////object
+//const mhs = {
+//    nama: "budi",
+//    nim: "a111293192",
+//    gender: "laki",
+//    hobby: ["mancing", "tawuran", "ngegame"],
+//    isActive: true
+//};
+
+//console.log(mhs);
+//const user = {};
+//user.username = "budilagi";
+//user.password = "inipasswordbudi";
+//console.log(user);
+
+//user.password = "berubah";
+//console.log(user);
+
+////array of object
+//const animals = [
+//    { name: "budi", species: "dog", class: {name:"mamalia"}},
+//    { name: "tono", species: "dog", class: {name:"mamalia"}},
+//    { name: "nemo", species: "fish", class: {name:"invertebrata"}},
+//    { name: "dory", species: "fish", class: {name:"invertebrata"}},
+//    { name: "james", species: "dog", class: {name:"mamalia"}}
+//]
+
+//console.log(animals);
+////function onlydog, yaitu looping ke animals=> yg diambil hanya species dog
+////const onlyDog
+////jika species = fish => maka ubah class name menjadi 'non-mamalia'
+
+///*const onlyDog = [];*/
+////for (var i = 0; i < animals.length; i++) {
+////    if (animals[i].species == "dog") {
+////        onlyDog.push(animals[i]);
+////    }
+////}
+
+///*onlyDog.push(animals.filter(animal => animal.species == "dog"));*/
+////console.log(onlyDog);
+
+//let detailAnimal;
+//detailAnimal = animals.map(animal => {
+//    return {
+//        name: animal.name,
+//        species: animal.species,
+//        isFish: animal.species == "fish" ? true:false
+//    }
+//})
+
+//console.log(detailAnimal);
+
+//let data = {
+//    series: [30,20],
+//    labels: ["cowok","cewek"]
+//}
+
+//jquery
+
+//$("#judul").click(function () {
+//    $("#judul").css("background-color", "red");
+//    $("#judul").html("Berubah!!")
+//})
+
+//AJAX => Asynchronous Javascript and XML
+$.ajax({
+    url: "https://pokeapi.co/api/v2/pokemon/",
+    //success: function (result) {
+    //    console.log(result);
+    //}
+}).done((result) => {
+    //console.log(result.results);
+    let temp = ""
+    //for (var i = 0; i < result.results.length; i++) {
+    //    temp += "<li>" + result.results[i].name +"</li>";
+    //}
+    //$("#listPoke").html(temp);
+
+    $.each(result.results, function (key, val) {
+        //old way
+        //temp += "<tr>\
+        //            <td>"+(key+1)+"</td>\
+        //            <td>"+val.name+"</td>\
+        //            <td>"+val.name+"</td>\
+        //        </tr>"
+        //jalan ninja ku / pakai backtick / literal template
+        temp += `<tr>
+                    <td>${key+1}</td>
+                    <td>${val.name}</td>
+                    <td><button class="btn btn-primary" onclick="detailPoke('${val.url}')" data-bs-toggle="modal" data-bs-target="#modalPokeDetail">Detail</button></td>
+                </tr>`
+    })
+    $("#tablePoke").html(temp);
+
+
+}).fail((err) => {
+    console.log(err);
+})
+
+
+function detailPoke(stringUrl) {
+    $.ajax({
+        url: stringUrl,
+        success: function (result) {
+            $(".modal-title").html(result.name);
+        }
+    })
+}
